0a1bddd4f5a7b54c786042b132fab2bf9cbef911
peter0749 <peter0749@users.noreply.github.com>
2016-07-16 00:25:13 +0800

    Add files via upload

---
+++ YoutubeParser.R
@@ -0,0 +1,41 @@
+rm(list=ls(all=TRUE))
+library(XML)
+library(bitops)
+library(RCurl)
+library(NLP)
+library(httr)
+
+Sys.setlocale("LC_ALL", "cht")
+
+alldata = read.csv('Fulllist.csv')
+
+youtubeSRC = 'https://www.youtube.com/results?q='
+yAppendURL = '&sp=CAM%253D'
+youtubeURL = ''
+
+fulldata = data.frame()
+
+for( i in 1:length(alldata$X))
+{
+  youtubeURL <- paste(youtubeSRC, alldata$Title[i], yAppendURL, sep='')
+  youtubeURL <- gsub(" ","%20",youtubeURL)
+  youtubeURL <- iconv(youtubeURL, "big5", "utf8")
+  #Encoding(youtubeURL) = "UTF-8"
+  print(youtubeURL)
+  URLExist = url.exists(youtubeURL)
+  print(URLExist)
+  if(URLExist)
+  {
+    html = getURL(youtubeURL, ssl.verifypeer = FALSE, encoding='UTF-8')
+    xml = htmlParse(html, encoding='UTF-8')
+    text = xpathSApply(xml,'//li/div/div/div[2]/div[2]/ul/li[2]/text()', sessionEncoding='utf8', xmlValue)
+    text = substring(text,6)
+    text <- gsub(",","",text)
+    testframe = as.data.frame(t(text)[1])
+    names(testframe) = c("Youtube Views")
+    testframe = cbind(alldata[i,-1],testframe)
+    fulldata = rbind(fulldata, testframe)
+  }
+}
+
+write.csv(fulldata,"Youtubelist.csv")
