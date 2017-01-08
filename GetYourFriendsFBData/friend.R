247ac8d15bf4bb69a21f6660d1c82b6b717ce956
Kuang-Yu Jeng <peter0749@users.noreply.github.com>
2017-01-08 16:34:45 +0800

    Add files via upload

--- GetYourFriendsFBData/friend.R
+++ GetYourFriendsFBData/friend.R
@@ -7,25 +7,25 @@ parser_id <- function(X) {
   return(s)
 }
 
-build_all_query <- function(X, token='') {
-  return(getUsers(X,token, private_info = TRUE))
-}
-
 download_all_pic <- function(X) {
-  print(X[,11])
-  curl::curl_download(X[,11],paste('./FaceImg/', X[,1],'-', X[,2], '.jpg', sep='', collapse=''))
+  if(!is.na(X[,11])) {
+    print(X[,11])
+    try(curl::curl_download(X[,11],paste('./FaceImg/', X[,1],'-', X[,2], '.jpg', sep='', collapse='')),silent=TRUE)
+  }
 }
 
 if(!dir.exists('./FaceImg')){
   dir.create('./FaceImg')
 }
 
-#token = ''
+token = ''
 friend_ajax = read.table('friend_list.txt',header=FALSE)
 friend_ids = sapply(friend_ajax, parser_id)
 friend_status = data.frame()
 for(i in 1:nrow(friend_ids)) {
-  friend_status = rbind(friend_status, build_all_query(friend_ids[i,],token))
-  download_all_pic(friend_status[i,])
+  sig <- try((temp = getUsers(friend_ids[i],token, private_info = TRUE)), silent=FALSE)
+  if(class(sig)=='try-error') next
+  friend_status = rbind(friend_status, temp)
+  download_all_pic(temp)
 }
 write.csv(x = friend_status, file = 'myfriends.csv', fileEncoding = 'UTF-8')
